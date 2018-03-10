d7c0bc2c300ec5fe0a1cc6cbb85083f1cd2e504d
vippulpanditgit <vippul@cox.net>
2018-03-09 18:23:23 -0800

    code cleanup...

--- src/main/java/com/myreward/engine/webapp/MyRewardMetaDataHandler.java
+++ src/main/java/com/myreward/engine/webapp/MyRewardMetaDataHandler.java
@@ -2,51 +2,41 @@ package com.myreward.engine.webapp;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.myreward.engine.event.processor.EventProcessor;
 import com.myreward.engine.event.processor.MetaOpCodeProcessor;
 import com.myreward.engine.model.RuleRequestDO;
 import com.myreward.engine.rule.MyRewardMetaDataHashTable;
-import com.myreward.parser.generator.MyRewardDataSegment;
 
 @RestController
-@RequestMapping("/rule")
 public class MyRewardMetaDataHandler {
-
-	private RuleRequestDO ruleRequestDO;
-	
 	public MyRewardMetaDataHandler() {
-		ruleRequestDO = new RuleRequestDO();
-		ruleRequestDO.setMetaOpCodeProcessor(new MetaOpCodeProcessor());
-		ruleRequestDO.getMetaOpCodeProcessor().initialize();
-/*		metaOpCodeProcessor.parse(reward_metadata);
-		metaOpCodeProcessor.print_code_segment();
-        MyRewardDataSegment myRewardDataSegment = eventProcessor.createDataSegment();
-        eventProcessor.setMyRewardDataSegment(myRewardDataSegment);
-*/
+
 	}
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/rule", method = RequestMethod.GET)
 	public String get() {
 		return "TEST";
 	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public String set(@RequestBody String rule) {
+	@RequestMapping(value="/rule", method = RequestMethod.POST)
+	public ResponseEntity<String> set(@RequestBody String rule) {
 		try {
+			RuleRequestDO ruleRequestDO = new RuleRequestDO();
+			ruleRequestDO.setMetaOpCodeProcessor(new MetaOpCodeProcessor());
+			ruleRequestDO.getMetaOpCodeProcessor().initialize();
 			ruleRequestDO.setRule(rule);
 			ruleRequestDO.setRuleId(UUID.randomUUID().toString());
 			ruleRequestDO.getMetaOpCodeProcessor().parse(rule);
 			ruleRequestDO.getMetaOpCodeProcessor().initialize();
 			MyRewardMetaDataHashTable.getInstance().getMetaDataHashTable().put(ruleRequestDO.getRuleId(), ruleRequestDO);
-			return ruleRequestDO.getRuleId();
+			return new ResponseEntity<String>(ruleRequestDO.getRuleId(), HttpStatus.OK);
 		} catch(Exception exp) {
-			exp.printStackTrace();
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
 		}
-		return null;
 	}
 	
 }
